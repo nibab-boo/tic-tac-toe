f2d1ba56101d562a1677fe844c9266cb811e408e
Babin Bohora <bohorababin1@gmail.com>
2022-05-09 01:32:16 +0900

    Updates

--- src/components/TickBox.js
+++
@@ -1,9 +0,0 @@
-import { useClickContextCheck } from './MainBox'
-const TickBox = ({ row, col }) => {
-  const handleClick = useClickContextCheck();
-  return (
-    <td data-row={row} data-col={col} onClick={(e)=>{ handleClick(e) }}></td>
-  );
-};
-
-export default TickBox;
