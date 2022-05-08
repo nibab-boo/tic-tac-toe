07a3aade3f5b2be9805be3af8f8bba42f6b4c106
Babin Bohora <bohorababin1@gmail.com>
2022-05-08 19:53:37 +0900

    removed unnecessary files and made basic structure

---
+++ src/components/MainBox.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const MainBox = () => {
+  return (
+    <div>
+      <table>
+        <tr>
+          <td>apple</td>
+          <td>apple</td>
+          <td>apple</td>
+        </tr>
+        <tr>
+          <td>apple</td>
+          <td>apple</td>
+          <td>apple</td>
+        </tr>
+        <tr>
+          <td>apple</td>
+          <td>apple</td>
+          <td>apple</td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default MainBox;
