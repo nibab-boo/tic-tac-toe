149af46e0331d0187ba1fb8753049172b9b2f476
Babin Bohora <bohorababin1@gmail.com>
2022-05-09 01:17:35 +0900

    refactored function and minimized too verbal code and comments

--- src/components/MainBox.js
+++ src/components/MainBox.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {createContext, useContext} from 'react';
 import TickRow from './TickRow';
 import { useTurn, useTurnUpdate } from './TurnProvider'
+
+const ClickHandleContext = createContext();
+export const ClickContextCheck = () => {
+  return useContext(ClickHandleContext);
+}
 
 const MainBox = () => {
   // Context from useContext.
@@ -8,46 +13,32 @@ const MainBox = () => {
   const turnToggle = useTurnUpdate();
 
   // Game Checker
-  const checkGame = (e) => {
-    const row = e.currentTarget.dataset.row;
-    const col = e.currentTarget.dataset.col;
-    let game = false;
+  const checkGame = (row, col) => {
     const rowBoxes = document.querySelectorAll(`[data-row="${row}"`);
     const colBoxes = document.querySelectorAll(`[data-col="${col}"`);
-    // Horizontal Check
-    // 1. Row remains same and only Columns change.
-    const horizontalCheck = () => {
-      game = Array.from(rowBoxes).every(td => td.dataset.turn === `${turn}`)
-      if (game) Array.from(rowBoxes).forEach((td)=> {td.style.background="green"})
+
+    const checkBoxes = (boxes) => {
+      if (boxes.every(td => td.dataset.turn === `${turn}`)) {
+        boxes.forEach((td)=> {td.style.background="green"});
+        return
+      }
     }
-    horizontalCheck();
+      // Horizontal Check
+    checkBoxes(Array.from(rowBoxes));
     // Vertical Check
-    // 1. Column remains same and only Rows change.
-    const verticalCheck = () => {
-      game = Array.from(colBoxes).every(td => td.dataset.turn === `${turn}`)
-      if (game) Array.from(colBoxes).forEach((td)=> {td.style.background="green"})
-    }
-    verticalCheck();
+    checkBoxes(Array.from(colBoxes));
     // Cross Check
     const crossCheck = () => {
       // return if e.currentTarget is one of this four box.
       const skipThese= ['1x2', '2x1','2x3', '3x2'];
       if (skipThese.includes(`${row}x${col}`)) return
-      // right slash check
-      const boxes = [];
+      const rightBoxes = [], leftBoxes = [];
       for (let i = 1; i <= 3; i++ ) {
-        boxes.push(document.querySelector(`[data-row="${i}"][data-col="${i}"]`))
+        rightBoxes.push(document.querySelector(`[data-row="${i}"][data-col="${i}"]`))
+        leftBoxes.push(document.querySelector(`[data-row="${i}"][data-col="${4 - i}"]`))
       }
-      game = (boxes).every(td => td.dataset.turn === `${turn}`)
-      if (game) (boxes).forEach((td)=> {td.style.background="green"})
-      // left slash check
-      boxes.length = 0;
-      for (let i = 1; i <= 3; i++ ) {
-        boxes.push(document.querySelector(`[data-row="${i}"][data-col="${4 - i}"]`))
-        console.log(boxes);
-      }
-      game = (boxes).every(td => td.dataset.turn === `${turn}`)
-      if (game) (boxes).forEach((td)=> {td.style.background="green"})
+      checkBoxes(rightBoxes);
+      checkBoxes(leftBoxes);
     }
     crossCheck();
   }
@@ -56,17 +47,11 @@ const MainBox = () => {
   // Handling Click event.
   const handleClick = (e) => {
     if (e.currentTarget.dataset.turn) return
-    if (turn) {
-      e.currentTarget.textContent = "X";
-    } else {
-      e.currentTarget.textContent = "O"
-    }
+    turn ? e.currentTarget.textContent = "X" : e.currentTarget.textContent = "O" ;
     e.currentTarget.dataset.turn = turn;
-    checkGame(e);
+    checkGame( e.currentTarget.dataset.row, e.currentTarget.dataset.col);
     turnToggle();
   }
-
-
 
   // Creating rows.
   const rows = [];
@@ -75,13 +60,13 @@ const MainBox = () => {
   }
 
   return (
-    <div>
+    <ClickHandleContext.Provider>
       <table>
         <tbody>
           { rows }
         </tbody>
       </table>
-    </div>
+    </ClickHandleContext.Provider>
   );
 };
 
