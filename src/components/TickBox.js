3abe434d0cd730556355d024312bcbaed77d9cd4
Babin Bohora <bohorababin1@gmail.com>
2022-05-09 01:22:44 +0900

    removed space

--- src/components/TickBox.js
+++ src/components/TickBox.js
@@ -2,9 +2,7 @@ import { useClickContextCheck } from './MainBox'
 const TickBox = ({ row, col }) => {
   const handleClick = useClickContextCheck();
   return (
-    <td data-row={row} data-col={col} onClick={(e)=>{ handleClick(e) }}>
- 
-    </td>
+    <td data-row={row} data-col={col} onClick={(e)=>{ handleClick(e) }}></td>
   );
 };
 
